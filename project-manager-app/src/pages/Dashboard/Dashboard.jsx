16c3225c79d2bb65cbc70a0f6f6e651855b9fef8
JorgeML01 <jorgedd2001@gmail.com>
2023-05-28 23:30:46 -0600

    jsonwebtoken decoded - alternative

--- project-manager-app/src/pages/Dashboard/Dashboard.jsx
+++ project-manager-app/src/pages/Dashboard/Dashboard.jsx
@@ -1,37 +1,28 @@
 import React from "react";
 import "./styles.css";
 
+function decodeToken(token) {
+  const payload = token.split(".")[1];
+  const decodedPayload = atob(payload);
+  const parsedPayload = JSON.parse(decodedPayload);
+  return parsedPayload;
+}
+
 function Dashboard() {
+  const token = localStorage.getItem("accessToken");
+  const decodedToken = decodeToken(token);
+  const email = decodedToken.email;
+
   return (
     <section id="dashboard" role="dashboard">
       <div className="container">
         <div className="board">
           <h2>Board name</h2>
           <p>Board description</p>
-        </div>
-        <div className="board">
-          <h2>Board name</h2>
-          <p>Board description</p>
-        </div>
-        <div className="board">
-          <h2>Board name</h2>
-          <p>Board description</p>
-        </div>
-        <div className="board">
-          <h2>Board name</h2>
-          <p>Board description</p>
-        </div>
-        <div className="board">
-          <h2>Board name</h2>
-          <p>Board description</p>
-        </div>
-        <div className="board">
-          <h2>Board name</h2>
-          <p>Board description</p>
+          <p>Your email: {email}</p>
         </div>
       </div>
     </section>
   );
 }
-
 export default Dashboard;
